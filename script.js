919f5bcc5ba29ac42087aafcaef161eb8e0b7515
X.Nimesh <nimush14@gmail.com>
2022-10-23 22:39:51 +0545

    delete feature added

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ let taskAdd = () => {
 
     // retrieving the latest task id
     let taskList = document.getElementById('taskList');
-    let id = taskList.childNodes.length - 2;
+    let taskCount = taskList.childElementCount - 1;
+    let taskId;
+    taskCount === -1 ?
+        taskId = 0 :
+        taskId = parseInt(taskList.children[taskCount].id) + 1;
 
     // creating element 
     let div = document.createElement('div');
     div.classList.add('task', 'new-task');
-    div.id = id;
+    div.id = taskId;
 
     let p = document.createElement('p');
     p.innerHTML = task.value;
@@ -28,13 +32,24 @@ let taskAdd = () => {
     let deleteButton = document.createElement('button');
     deleteButton.className = 'deleteButton';
     deleteButton.innerHTML = 'Delete';
+    deleteButton.id = taskId;
+
+    deleteButton.setAttribute("onclick", "deleteTask(event)");
     innerDiv.appendChild(deleteButton)
 
     div.appendChild(innerDiv);
     taskList.appendChild(div);
 
 }
+let deleteTask = (event) => {
+    if (confirm('Are you sure you want to delete the task?')) {
+        console.log(event.target.id);
+        let taskId = event.target.id;
+        document.getElementById(taskId).remove();
+    }
 
+}
 document.getElementById("submit-Task").addEventListener("click", taskAdd);
+
 
 
