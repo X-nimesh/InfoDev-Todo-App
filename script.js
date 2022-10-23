1ee4f82d71c38e011d1039cd8ec982b00e472360
X.Nimesh <nimush14@gmail.com>
2022-10-23 19:22:35 +0545

    latest task id retrieved and added in new element

--- script.js
+++ script.js
@@ -2,15 +2,39 @@
 let taskAdd = () => {
     let task = document.getElementById('create-task');
     console.log(task.value);
-    // constnewdiv = document.createElement('div');
 
-    let descrip = document.createElement("li");
-    let text = document.createTextNode(task.value);
-    descrip.appendChild(text);
-    document.getElementById("taskList").appendChild(descrip);
-    // taskCheck.setAttribute('type', 'checkbox');
+    // retrieving the latest task id
+    let taskList = document.getElementById('taskList');
+    let id = taskList.childNodes.length - 2;
+
+    // creating element 
+    let div = document.createElement('div');
+    div.classList.add('task', 'new-task');
+    div.id = id;
+
+    let p = document.createElement('p');
+    p.innerHTML = task.value;
+    p.className = 'task-descip';
+    div.appendChild(p);
+
+    let innerDiv = document.createElement('div');
+    innerDiv.className = 'buttons';
+
+    let editButton = document.createElement('button');
+    editButton.className = 'editButton';
+    editButton.innerHTML = 'Edit';
+    innerDiv.appendChild(editButton);
+
+    let deleteButton = document.createElement('button');
+    deleteButton.className = 'deleteButton';
+    deleteButton.innerHTML = 'Delete';
+    innerDiv.appendChild(deleteButton)
+
+    div.appendChild(innerDiv);
+    taskList.appendChild(div);
+
 }
 
-
-// document.getElementById("create-task").addEventListener("change", taskAdd);
 document.getElementById("submit-Task").addEventListener("click", taskAdd);
+
+
